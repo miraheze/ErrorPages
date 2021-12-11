380d48ee71c45f6fbd565854f5541bfeed90913d
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-12-11 15:21:23 -0700

    Update getTranslations.php

--- getTranslations.php
+++ getTranslations.php
@@ -1,20 +1,9 @@
 <?php
 
-$lang = Locale::acceptFromHttp( $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? 'en' );
-$i18nDirectory = scandir( 'i18n' );
-
-unset( $i18nDirectory['.'], $i18nDirectory['..'], $i18nDirectory['qqq.json'] );
-
-if ( in_array( "{$lang}.json", $i18nDirectory ) ) {
-	$translations = json_decode( file_get_contents( __DIR__ . "/i18n/{$lang}.json" ), true );
-} else {
-	$translations = json_decode( file_get_contents( __DIR__ . "/i18n/en.json" ), true );
-}
-
-unset( $translations['@metadata'] );
-
 function getTranslation( $key ) {
-	global $translations;
+	if ( getLanguageCode() === 'qqx' ) {
+		return "({$key})";
+	}
 	
 	switch ( json_last_error() ) {
 		case JSON_ERROR_NONE:
@@ -47,10 +36,56 @@ function getTranslation( $key ) {
 		throw new Exception( $error );
 	}
 
-	if ( isset( $translations[$key] ) && $translations[$key] ) {
-		return $translations[$key];
-	}  else {	
-		return json_decode( file_get_contents( __DIR__ . "/i18n/en.json" ), true )[$key];
-	}
+	return getLocalisation()[$key] ?? getFallback()[$key] ?? getDefault()[$key];
 }
+
+function getLanguageCode() {
+	$languageCode = Locale::acceptFromHttp( $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? 'en' );
+
+	if ( $languageCode === 'qqx' ) {
+		return $languageCode;
+	}
+
+	$basePath = __DIR__ . '/i18n/';
+	$realBasePath = realpath( $basePath );
+
+	$languagePath = $basePath . "{$languageCode}.json";
+	$realLanguagePath = realpath( $languagePath );
+
+	// Check for path traversal.
+	// Force language to 'en' if attempt was found.
+	if ( $realLanguagePath === false || strpos( $realLanguagePath, $realBasePath ) !== 0 ) {
+		$languageCode = 'en';
+	}
+
+	return $languageCode;
+}
+
+function getLocalisation() {
+	$lang = getLanguageCode();
+	if ( file_exists( __DIR__ . "/i18n/{$lang}.json" ) && $lang !== 'qqq' ) {
+		$translations = json_decode( file_get_contents( __DIR__ . "/i18n/{$lang}.json" ), true );
+	} else {
+		$translations = getDefault();
+	}
+
+	unset( $translations['@metadata'] );
+
+	return $translations;
+}
+
+function getFallback() {
+	$fallback = LOCALE_GET_PRIMARY_LANGUAGE( getLanguageCode() );
+
+	if ( file_exists( __DIR__ . "/i18n/{$fallback}.json" ) ) {
+		return json_decode( file_get_contents( __DIR__ . "/i18n/{$fallback}.json" ), true );
+	}
+
+	return getDefault();
+}
+
+function getDefault() {
+	return json_decode( file_get_contents( __DIR__ . '/i18n/en.json' ), true );
+}
+
 ?>
