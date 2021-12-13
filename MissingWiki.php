5a79c6013b6e04aa1b808cce70fd7e5b22efdbb9
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-12-13 14:40:18 -0700

    T8005: Add localisation to error pages (#2)

--- MissingWiki.php
+++ MissingWiki.php
@@ -1,6 +1,11 @@
 <?php
 
 if ( !$wgCommandLineMode ) {
+	require_once __DIR__ . '/getTranslations.php';
+
+	$getLanguageCode = 'getLanguageCode';
+	$getTranslation = 'getTranslation';
+
 	http_response_code( 404 );
 	$requestURL = htmlspecialchars( $_SERVER['REQUEST_URI'] );
 	date_default_timezone_set( 'UTC' ); // Set to UTC +0
@@ -8,12 +13,12 @@ if ( !$wgCommandLineMode ) {
 
 	echo <<<EOF
 		<!DOCTYPE html>
-		<html lang="en">
+		<html lang="{$getLanguageCode()}">
 			<head>
 				<meta charset="utf-8">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<meta name="description" content="Wiki not Found">
-				<title>Wiki not Found</title>
+				<title>{$getTranslation( 'missingwiki' )}</title>
 				<!-- Bootstrap core CSS -->
 				<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
 				<style>
@@ -52,21 +57,21 @@ if ( !$wgCommandLineMode ) {
 			<div class="container">
 				<!-- Jumbotron -->
 				<div class="jumbotron">
-					<h1><img src="https://static.miraheze.org/metawiki/3/35/Miraheze_Logo.svg" alt="Miraheze Logo">404 Wiki not Found</h1>
-					<p class="lead">We couldn't find the wiki you were looking for on our servers.</p>
+					<h1><img src="https://static.miraheze.org/metawiki/3/35/Miraheze_Logo.svg" alt="Miraheze Logo">404 {$getTranslation( 'missingwiki' )}</h1>
+					<p class="lead">{$getTranslation( 'wiki-not-found' )}</p>
 					<p>
-						<a href="https://meta.miraheze.org/wiki/Special:MyLanguage/Miraheze" class="btn btn-lg btn-outline-success" role="button">Take me to Miraheze Meta</a>
+						<a href="https://meta.miraheze.org/wiki/Special:MyLanguage/Miraheze" class="btn btn-lg btn-outline-success" role="button">{$getTranslation( 'wiki-not-found-meta' )}</a>
 					</p>
 				</div>
 			</div>
 			<div class="container">
 				<div class="body-content">
 					<div style="text-align:center;">
-						<h2>What can I do?</h2>
-						<p class="lead">If you're a wiki visitor:</p>
-						<p>This wiki does not exist on our servers. You can browse wikis on our network <a href="https://meta.miraheze.org/wiki/Special:WikiDiscover">here</a> or <a href="https://archive.org/search.php?query=miraheze">view archive.org archives</a>.</p>
-						<p class="lead">If you're the wiki bureaucrat:</p>
-						<p>If this is a problem, please <a href="https://meta.miraheze.org/wiki/Special:MyLanguage/Help_center">contact us</a>.</p>
+						<h2>{$getTranslation( 'wiki-not-found-more' )}</h2>
+						<p class="lead">{$getTranslation( 'wiki-not-found-visitor' )}</p>
+						<p>{$getTranslation( 'wiki-not-found-browse' )}</p>
+						<p class="lead">{$getTranslation( 'wiki-not-found-bureaucrat' )}</p>
+						<p>{$getTranslation( 'wiki-not-found-contact' )}</p>
 					</div>
 				</div>
 			</div>
