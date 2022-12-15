877e73ebaacf7f0d9d7b0f3e9c984adcb3bbd581
paladox <paladox@users.noreply.github.com>
2022-12-15 22:41:46 +0000

    Revert "Add Content-Length to Missing Wikis" (#27)
    Revert "Add Content-Length to Missing Wikis (#26)"
    
    This reverts commit 3ec0ea3f0726410680b93bf2b52be876c7137481.

--- MissingWiki.php
+++ MissingWiki.php
@@ -8,7 +8,7 @@ if ( !$wgCommandLineMode ) {
 
 	http_response_code( 404 );
 
-	$output = <<<EOF
+	echo <<<EOF
 		<!DOCTYPE html>
 		<html lang="{$getLanguageCode()}">
 			<head>
@@ -88,8 +88,7 @@ if ( !$wgCommandLineMode ) {
 			</div>
 		</html>
 	EOF;
-	header( 'Content-length: ' . strlen( $output ) );
-	echo $output;
+
 	die( 1 );
 } else {
 	// $wgDBname will always be set to a string, even if the --wiki parameter was not passed to a script.
